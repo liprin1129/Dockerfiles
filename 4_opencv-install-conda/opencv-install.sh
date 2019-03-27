21d23c3d0be44ace8e8e4e8981cd9ffe5b7aa61c
liprin1129 <liprin1129@gmail.com>
2019-03-27 16:58:32 +0900

    opencv after conda

---
+++ 4_opencv-install-conda/opencv-install.sh
@@ -0,0 +1,14 @@
+sudo apt-get install --assume-yes build-essential cmake git
+sudo apt-get install --assume-yes pkg-config unzip ffmpeg qtbase5-dev python-dev python3-dev python-numpy python3-numpy
+sudo apt-get install --assume-yes libopencv-dev libgtk-3-dev libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev
+sudo apt-get install --assume-yes libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get install --assume-yes libv4l-dev libtbb-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev
+sudo apt-get install --assume-yes libvorbis-dev libxvidcore-dev v4l-utils vtk6
+sudo apt-get install --assume-yes liblapacke-dev libopenblas-dev libgdal-dev checkinstall
+
+sudo mkdir build
+cd build
+
+sudo cmake -DENABLE_CX11=ON -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D FORCE_VTK=ON -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_CUBLAS=ON -D CUDA_NVCC_FLAGS="-D_FORCE_INLINES" -D WITH_GDAL=ON -D WITH_XINE=ON -D BUILD_EXAMPLES=ON ..
+
+sudo ln -s /usr/local/cuda/extras/CUPTI/include/GL/glx.h /usr/local/include/GL/glx.h
