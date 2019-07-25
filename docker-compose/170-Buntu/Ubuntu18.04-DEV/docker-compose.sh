bcae07d7293b39a384deaaa672305bbf31a28db5
liprin1129 <liprin1129@gmail.com>
2019-07-25 15:47:49 +0900

    ZED camera can be used

--- docker-compose/170-Buntu/Ubuntu18.04-DEV/docker-compose.sh
+++ docker-compose/170-Buntu/Ubuntu18.04-DEV/docker-compose.sh
@@ -2,6 +2,7 @@ docker run \
        --gpus all \
        --rm -it \
        --env=DISPLAY \
+       --privileged \
        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
        -v /home/user170/Downloads:/home/Downloads \
        -v /mnt/08F8996AF8995732/DEVs:/DEVs -v \
