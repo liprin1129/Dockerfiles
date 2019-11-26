97909ce58b9579b1b575fb831b438edac056b077
liprin1129 <liprin1129@gmail.com>
2019-11-26 14:10:37 +0900

    aa

--- docker-compose/170-Buntu/Ubuntu18.04-DEV/docker-compose.sh
+++ docker-compose/170-Buntu/Ubuntu18.04-DEV/docker-compose.sh
@@ -6,7 +6,8 @@ docker run \
        --ipc host \
        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
        -v /home/user170/Downloads:/home/Downloads \
-       -v /mnt/08F8996AF8995732/DEVs:/DEVs -v \
-       /mnt/08F8996AF8995732/DATASETs:/DATASETs liprin1129/ubuntu18.04:cudagl10.0-cudnn7.5-boost-qt5-pytorch-opencv-zed-code
+       -v /mnt/08F8996AF8995732/DEVs:/DEVs \
+       -v /mnt/08F8996AF8995732/DATASETs:/DATASETs \
+       liprin1129/ubuntu18.04:cudagl10.0-cudnn7.5-boost-qt5-pytorch-opencv-zed-code
        #/mnt/08F8996AF8995732/DATASETs:/DATASETs liprin1129/ubuntu18.04:cudagl10.0-cudnn7.5-boost-qt5-pytorch-opencv-zed-code-openpose
        #/mnt/08F8996AF8995732/DATASETs:/DATASETs liprin1129/ubuntu18.04:rl
